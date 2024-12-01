aeb1564cdfd1a72d327332ecd3850c51e48480ff
steve kay <stevekay@gmail.com>
2024-12-01 21:49:00 +0000

    01

---
+++ 01/01.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import fileinput
+import re
+
+listone, listtwo = [], []
+
+for line in fileinput.input():
+    listone.append(int(line.split(None)[0]))
+    listtwo.append(int(line.split(None)[1]))
+
+listone.sort()
+listtwo.sort()
+
+partone = parttwo = 0
+for a in range(len(listone)):
+    partone += abs(listone[a] - listtwo[a])
+    parttwo += listtwo.count(listone[a]) * listone[a]
+
+print(partone,parttwo)
---
+++ 01/README.md
@@ -0,0 +1,6 @@
+
+[steve@ryzen 01]$ ./01.py <example.txt
+11 31
+[steve@ryzen 01]$ ./01.py <input.txt
+2367773 21271939
+[steve@ryzen 01]$
