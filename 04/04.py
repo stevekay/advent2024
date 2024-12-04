2eb7d74a4229705687cc5a3c8b2aa094728a8a97
steve kay <stevekay@gmail.com>
2024-12-04 13:03:55 +0000

    04

--- 04/04.py
+++ 04/04.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import fileinput
+import re
 
 partone = 0
 parttwo = 0
@@ -10,7 +11,7 @@ a=[]
 rows=0
 
 for line in fileinput.input():
-    cols=len(line)-1
+    cols = len(line)-1
     a.append([])
     for col in range(0,cols):
         a[rows].append(line[col])
@@ -18,33 +19,16 @@ for line in fileinput.input():
 
 for y in range(0,rows):
     for x in range(0,cols):
-        # M S
-        #  A
-        # M S
-        if x < cols-2 and y < rows-2 and a[y][x]=='M' and a[y][x+2]=='S' and a[y+1][x+1]=='A' and a[y+2][x]=='M' and a[y+2][x+2]=='S':
-            parttwo+=1
-        # S S
-        #  A
-        # M M
-        if x < cols-2 and y < rows-2 and a[y][x]=='S' and a[y][x+2]=='S' and a[y+1][x+1]=='A' and a[y+2][x]=='M' and a[y+2][x+2]=='M':
-            parttwo+=1
-        # M M
-        #  A
-        # S S
-        if x < cols-2 and y < rows-2 and a[y][x]=='M' and a[y][x+2]=='M' and a[y+1][x+1]=='A' and a[y+2][x]=='S' and a[y+2][x+2]=='S':
-            parttwo+=1
-        # S M
-        #  A
-        # S M
-        if x < cols-2 and y < rows-2 and a[y][x]=='S' and a[y][x+2]=='M' and a[y+1][x+1]=='A' and a[y+2][x]=='S' and a[y+2][x+2]=='M':
-            parttwo+=1
+        if x < cols-1 and y > 0 and y < rows-1 and a[y][x]=='A':
+            if re.match(r'^(MSMS|SSMM|MMSS|SMSM)$', a[y-1][x-1] + a[y-1][x+1] + a[y+1][x-1] + a[y+1][x+1]):
+                parttwo += 1
 
         for xd in [ -1, 0, 1 ]:
             for yd in [ -1, 0, 1]:
-                found=1
+                found = 1
                 for l in range(0,len(target)):
                     if x+xd*l not in range (0, cols) or y+yd*l not in range (0, rows) or a[y+yd*l][x+xd*l] != target[l]:
-                        found=0
+                        found = 0
                 partone += found
 
-print(partone,parttwo)
+print(partone, parttwo)
