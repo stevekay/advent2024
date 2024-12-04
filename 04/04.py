f495e5b19e90c2f7e276c7b98cf4bc42d4ea2780
steve kay <stevekay@gmail.com>
2024-12-04 14:02:06 +0000

    04

--- 04/04.py
+++ 04/04.py
@@ -10,8 +10,8 @@ a=[]
 for b in open(0).readlines():
     a.append(list(b.strip()))
 
-for y in range(0,len(a)):
-    for x in range(0,len(a[0])):
+for y in range(len(a)):
+    for x in range(len(a[0])):
         if x < len(a[0])-1 and y and y < len(a)-1:
             parttwo += bool(re.match(r'^(MSAMS|SSAMM|MMASS|SMASM)$', \
               a[y-1][x-1] + a[y-1][x+1] + a[y][x] + \
@@ -20,7 +20,7 @@ for y in range(0,len(a)):
         for xd in [ -1, 0, 1 ]:
             for yd in [ -1, 0, 1]:
                 found = 1
-                for l in range(0, len(target)):
+                for l in range(len(target)):
                     if x+xd*l not in range (0, len(a[0])) or \
                        y+yd*l not in range (0, len(a)) or \
                        a[y+yd*l][x+xd*l] != target[l]:
