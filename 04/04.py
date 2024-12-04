b0705a551d14bfdfccc94165c33dbb4a07828e62
steve kay <stevekay@gmail.com>
2024-12-04 13:30:19 +0000

    day4

--- 04/04.py
+++ 04/04.py
@@ -3,23 +3,19 @@
 import fileinput
 import re
 
-partone = 0
-parttwo = 0
+partone = parttwo = 0
 target='XMAS'
 
 a=[]
-rows=0
 
-for line in fileinput.input():
-    cols = len(line)-1
-    a.append([])
-    for col in range(0,cols):
-        a[rows].append(line[col])
-    rows += 1
+for b in open(0).readlines():
+    a.append(list(b.strip()))
 
-for y in range(0,rows):
+cols=len(a[0])
+
+for y in range(0,len(a)):
     for x in range(0,cols):
-        if x < cols-1 and y > 0 and y < rows-1 and a[y][x]=='A':
+        if x < cols-1 and y > 0 and y < len(a)-1 and a[y][x]=='A':
             if re.match(r'^(MSMS|SSMM|MMSS|SMSM)$', a[y-1][x-1] + a[y-1][x+1] + a[y+1][x-1] + a[y+1][x+1]):
                 parttwo += 1
 
@@ -27,7 +23,7 @@ for y in range(0,rows):
             for yd in [ -1, 0, 1]:
                 found = 1
                 for l in range(0,len(target)):
-                    if x+xd*l not in range (0, cols) or y+yd*l not in range (0, rows) or a[y+yd*l][x+xd*l] != target[l]:
+                    if x+xd*l not in range (0, cols) or y+yd*l not in range (0, len(a)) or a[y+yd*l][x+xd*l] != target[l]:
                         found = 0
                 partone += found
 
